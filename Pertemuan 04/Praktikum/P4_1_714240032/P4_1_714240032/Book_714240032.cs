c153b0b1f78fbb694bdfbf0985c89f59cd80102e
Mochammad Fauzi Nur Fadilah <nurfadilahmfauzi@gmail.com>
2025-11-03 16:27:48 +0700

    push p4_1_714240032

---
+++ Pertemuan 04/Praktikum/P4_1_714240032/P4_1_714240032/Book_714240032.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714240032
+{
+    public class Book_714240032 : Product_714240032
+    {
+        protected string pageCount;
+
+        public Book_714240032(string type, string title, string pagecount)
+            : base(type, title)
+        {
+            this.pageCount = pagecount;
+        }
+
+        public string PageCount
+        {
+            get { return pageCount; }
+            set { pageCount = value; }
+        }
+
+        // Implementasi metode abstrak
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Product is a {0} called \"{1}\" and it has {2} pages", MyType, MyTitle, PageCount);
+        }
+    }
+}
---
+++ Pertemuan 04/Praktikum/P4_1_714240032/P4_1_714240032/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714240032
+{
+     internal class ProductTest_714240032
+    {
+        static void Main(string[] args)
+        {
+            Book_714240032 Product1 = new Book_714240032("Book", "#C object Oriented Solution", "300");
+            DVD_714240032 Product2 = new DVD_714240032("Ethernal Sunshine Of The Spotles Mind ", "145");
+
+            Product1.DisplayInfo();
+            Product2.DisplayInfo();
+        }
+    }
+}
+    
